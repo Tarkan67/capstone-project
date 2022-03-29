69176a308cdc3c9d4f4d0f1973705dd1c31962e1
Tarkan Aldirmaz <tarkan-aldirmaz@web.de>
2022-03-29 14:06:40 +0200

    added Ref hook to scroll to the map and top

--- pages/index.js
+++ pages/index.js
@@ -20,64 +20,70 @@ const MapEffect = ({ useMap }) => {
 };
 
 export default function Home() {
-  const myRef = useRef(null);
-  const executeScroll = () => myRef.current.scrollIntoView();
+  const myRefMap = useRef(null);
+  const executeScrollToMap = () => myRefMap.current.scrollIntoView();
+  const myRefTop = useRef(null);
+  const executeScrollToTop = () => myRefTop.current.scrollIntoView();
 
   return (
     <div className={styles.relativeBox}>
-      <button className={styles.topBtn} onClick={executeScroll}>
+      <button className={styles.topBtn} onClick={executeScrollToTop}>
         Top
       </button>
       <button className={styles.submitBtn}>Submit</button>
-      <button className={styles.bottomBtn}>Bottom</button>
+      <button className={styles.bottomBtn} onClick={executeScrollToMap}>
+        Map
+      </button>
 
       <Head>
         <title>Elden Guesser</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image
-        src={locations[randomInteger(4)].path}
-        alt="First Picture"
-        width="2160"
-        height="1215"
-        className={styles.overlay}
-        ref={myRef}
-      />
-
-      <Map
-        className={styles.homeMap}
-        center={[40.5, 100.5]}
-        zoom={3}
-        bounds={[
-          [0, 0],
-          [50.5, 30.5],
-        ]}
-        fitBounds={[
-          [0, 0],
-          [50.5, 30.5],
-        ]}
-      >
-        {({ TileLayer, Marker, Popup, useMap, ImageOverlay }, map, rc) => (
-          <>
-            <MapEffect useMap={useMap} />
-            <TileLayer
-              url="/tiles/{z}/{x}/{y}.png"
-              noWrap
-              bounds={rc.getMaxBounds()}
-              maxNativeZoom={rc.zoomLevel()}
-            />
-            <Marker position={[40, 100]}>
-              <Popup>
-                <Image
-                  src="/location.png"
-                  alt="Picture of location"
-                  layout="fill"
-                />
-              </Popup>
-            </Marker>
-          </>
-        )}
-      </Map>
+      <div ref={myRefTop}>
+        <Image
+          src={locations[randomInteger(4)].path}
+          alt="First Picture"
+          width="2160"
+          height="1215"
+          className={styles.overlay}
+        />
+      </div>
+      <div ref={myRefMap}>
+        <Map
+          className={styles.homeMap}
+          center={[40.5, 100.5]}
+          zoom={3}
+          bounds={[
+            [0, 0],
+            [50.5, 30.5],
+          ]}
+          fitBounds={[
+            [0, 0],
+            [50.5, 30.5],
+          ]}
+        >
+          {({ TileLayer, Marker, Popup, useMap, ImageOverlay }, map, rc) => (
+            <>
+              <MapEffect useMap={useMap} />
+              <TileLayer
+                url="/tiles/{z}/{x}/{y}.png"
+                noWrap
+                bounds={rc.getMaxBounds()}
+                maxNativeZoom={rc.zoomLevel()}
+              />
+              <Marker position={[40, 100]}>
+                <Popup>
+                  <Image
+                    src="/location.png"
+                    alt="Picture of location"
+                    layout="fill"
+                  />
+                </Popup>
+              </Marker>
+            </>
+          )}
+        </Map>
+      </div>
     </div>
   );
 }
