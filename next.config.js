3b8d3516af2d48ea0de1580032bb7ecaa194445f
Tarkan Aldirmaz <tarkan-aldirmaz@web.de>
2022-04-04 17:20:35 +0200

    changed swcMinify to false

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  presets: ["next/babel"],
 };
 
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
 };
